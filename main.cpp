c26ee306e0c8215440f2b96f3f3137b8d535986c
roei <roeibenzeev@gmail.com>
2019-12-16 20:38:21 +0200

    Revert "asaf testing git 3"
    This reverts commit 4689793295f9869fb5377386ac79fcc43dbd2a80.
    
    # Conflicts:
    #	main.cpp

--- main.cpp
+++ main.cpp
@@ -3,5 +3,14 @@
 
 int main(int argc, char *argv[]) {
 
-    Lexer L = new Lexer(argv[0]);
+    Lexer *l = new Lexer(argv[argc-1]);
+    l->Lexing();
+    for (string v : l->myVec) {
+        if (!v.compare("EOL")) {
+            cout << endl;
+            continue;
+        }
+        cout << v << " ";
+
+    }
 }
