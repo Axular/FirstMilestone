6c5f4d4f1cc07abbb4958a7f2f65626cb19d75db
asaf <asaf.jerbi@gmail.com>
2019-12-29 16:27:54 +0200

    getter for var type

--- DefineVarCommand.cpp
+++ DefineVarCommand.cpp
@@ -6,11 +6,14 @@
 #include "DefineVarCommand.h"
 #include "Var.h"
 #include "VariablesSymbolTable.h"
+#include "Expression.h"
+#include "globals.h"
 
 
 /*this function creates new var*/
 void DefineVarCommand::execute(vector<string> executionCode) {
-
+    //todo: remove me
+    cout << "parser testing";
 /*    when we define a var we know the locations are in this order: (0)name, (1)type, (2) value
  *    examples:
  *    var starter -> sim("/controls/switches/starter")
@@ -20,24 +23,39 @@ void DefineVarCommand::execute(vector<string> executionCode) {
     //saving the data from the vector with names
     string name = executionCode[0];
     string type = executionCode[1];
-    string value = executionCode[3];
-    string sim = executionCode[3];
+    string valueStr = executionCode[2];
+    string sim = executionCode[2];
+
 
     //this variable is a double which is Not A Number (good for signing a double as an uninitialized double)
-    double NaN = numeric_limits<double>::quiet_NaN();
     Var newVariable;
 
     //create new var:
     if (type == "->") {
-        newVariable = Var(name, Var::VarType::OutputVar, NaN, sim);
+        newVariable = Var(name, Var::VarType::OutputVar, sim);
     } else if (type == "<-") {
-        newVariable = Var(name, Var::VarType::InputVar, NaN, sim);
+        newVariable = Var(name, Var::VarType::InputVar, sim);
     } else if (type == "=") {
-        newVariable = Var(name, Var::VarType::RegularVar, NaN, value);//todo: value.calculate()
+        Expression* exp = nullptr;
+        double value;
+        try {
+
+            exp = globalInterpreter->interpret(valueStr);
+            value = exp->calculate();
+            delete exp;
+            //delete globalInterpreter;
+        } catch (const char* e) {
+            if (exp != nullptr) {
+                delete exp;
+            }
+        }
+        newVariable = Var(name, Var::VarType::RegularVar, value);//todo: value.calculate()
     } else {
         cout << "Unexpected type of var: none of these were detected: =, ->, <-";
     }
 
     //add var to symbol table(NOTE: if var already exist, symbolTable will update it's value):
     VariablesSymbolTable::getInstance().insertVariable(newVariable);
+
+
 }
